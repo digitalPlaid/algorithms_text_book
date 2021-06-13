0c9fe8c62a5a9f34388141bb4312f08f7b1cb1b9
staubindj <staubindj@gmail.com>
2021-06-13 15:06:31 -0500

    idx error fxd. common cuz translating between 1 and 0 idx, book vs python

--- Algorithms Book/Chapter 2/3/merge_sort.py
+++ Algorithms Book/Chapter 2/3/merge_sort.py
@@ -8,16 +8,18 @@ def merge(lst,start,mid,end):
     right = []
     for i in range(length_one):
         left.append(lst[start+i])
-    for i in range(length_two):
-        right.append(lst[mid+j])
+    for i in range(length_two+1):
+        right.append(lst[mid+i])
     left.append(999999999)
     right.append(999999999)
     i = 0
     j = 0
-    for k in range(start,end):
+    for k in range(start,end+1):
         if left[i] <= right[j]:
+            lst[k] = left[i]
             i = i+1
-        elif lst[k] == right[j]:
+        else:
+            lst[k] = right[j]
             j = j+1
     return lst
 
@@ -26,4 +28,6 @@ def merge(lst,start,mid,end):
 # test case for merge:
 lst = [2,4,5,7,1,2,3,6]
 target = [1,2,2,3,4,5,6,7]
-print(len(lst), len(target))
+actual = merge(lst,0,4,7)
+print(actual)
+print(target==actual)
