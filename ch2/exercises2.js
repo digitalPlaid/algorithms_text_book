8212e40b521eebec24bf380c9eea15b806da8de6
Dan St. Aubin <staubindj@gmail.com>
2021-07-27 19:56:27 -0500

    finished thru exercise 2.2-2, selection sort

---
+++ ch2/exercises2.js
@@ -0,0 +1,35 @@
+// number 1:
+// Theta(n^3)
+
+// number 2:
+// write selection sort:
+// find smallest element, change with a[0], find next smallest, change with a[1]...
+// these algorithms are ones performed in place
+// not going to assume any native js functions
+
+function selectionSort(array) {
+    // find smallest in whole array
+    for (let i = 0; i < array.length-1; i++) {
+        let smallest = array[i];
+        let smallestIdx = i
+        for (let j = i+1; j < array.length; j++) {
+            if (array[j] < smallest) {
+                smallest = array[j];
+                smallestIdx = j;
+            }
+        }
+        array[smallestIdx] = array[i];
+        array[i] = smallest; 
+    }
+    return array
+}
+
+let ary = [5,2,4,6,1,3];
+let expected = [1,2,3,4,5,6];
+let actual = selectionSort(ary);
+console.log('final: ', actual)
+if (JSON.stringify(expected) === JSON.stringify(actual)) {
+    console.log('passed');
+} else {
+    console.log('failed')
+}
