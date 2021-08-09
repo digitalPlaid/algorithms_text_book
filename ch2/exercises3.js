23fa621a9b6af19f0d79cc444cdc1630f65f6eb2
Dan St. Aubin <staubindj@gmail.com>
2021-08-08 19:52:29 -0500

    onto the eoc problems

--- ch2/exercises3.js
+++ ch2/exercises3.js
@@ -48,6 +48,28 @@ if (JSON.stringify(unmerged) === JSON.stringify(goal)) {
 }
 
 // 2.3.3
+/*
+See the proof on page 37. No reason for this problem.
+*/
 
+// 2.3.4
+// pen and paper, but basically it takes constant time to if n is 1, and up to t(n-1) + insertion time for the last element
+// it takes O(n) for each call. it could take up to n recursive calls. 
+// whole thing is O(n^2), I think.
 
+// 2.3.5
+// binary search
+// informal argument. 
+// assume array is already sorted, at the minimal case, we have one element, there's nothing to split, it takes constant
+// time to tell if the element is there or not. 1 recursion. 
+// 2 elements. split the array in half, check each side. T(2) = split + 2*C
+// 3 elements. split into 1-2 then into 1,1-1, search each thing. 2(split) + 3*c, depth is 2
+// 4 elements. split into 2-2, then into 1-1,1-1, 2 depth, 2*split + 4*c
+// n elements split into n/2-n/2, then into n/4-n/4, n/4-n/4 etc. eventually you get to 2. that happens in lgn.
+// each time you split you only have to compare the end points around which you split, that's constant time.
+// clgn = O(lgn)
 
+// 2.3.6
+// no, still have to do the work of moving the subarray over, doesn't alleviate that problem
+
+// 2.3.7
